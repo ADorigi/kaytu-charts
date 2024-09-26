a2c1dfd299c57da76aa90902614a4d6595eee1eb
Adnan Gulegulzar <gulegulzaradnan@gmail.com>
2024-09-25 20:10:33 -0400

    feat: digitalocean

---
+++ .github/workflows/do_terraform/cluster.tf
@@ -0,0 +1,13 @@
+resource "digitalocean_kubernetes_cluster" "kaytu-charts" {
+  name   = "kaytu-charts"
+  region = "nyc1"
+  # Grab the latest version slug from `doctl kubernetes options versions`
+  version = "1.31.1-do.0"
+
+  node_pool {
+    name       = "kaytu-charts-node-pool"
+    size       = "g-2vcpu-8gb"
+    node_count = 3
+
+  }
+}
